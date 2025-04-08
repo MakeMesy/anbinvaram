193e15897f963c28e7ff9789803d00d37dfca5ae
bnaveenbharathi <naveenbharathi5050@gmail.com>
2025-04-08 08:03:31 +0530

    home page updated

--- index.php
+++ index.php
@@ -1,4 +1,3 @@
-
 <?php
 $page_title = "Anbin Varam";
 $page_description = "";
@@ -12,199 +11,506 @@ $page_image = "./assets/img/main/logo.webp";
 <html lang="en">
 
 <head>
-    <?php include('./resource/seo.php') ?>
+  <?php include('./resource/seo.php') ?>
 
-    <!-- cdns -->
-    <link
-  rel="stylesheet"
-  href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css"
-/>
-
+  <!-- cdns -->
+  <link
+    rel="stylesheet"
+    href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css" />
 
 
-    <!-- css -->
-    <link rel="stylesheet" href="./assets/css/home/style.css">
+
+  <!-- css -->
+  <link rel="stylesheet" href="./assets/css/home/style.css">
 </head>
 
 <body>
 
-    <?php include('./resource/header.php') ?>
-    <section class="divison_section">
-    <?php include('./resource/division.php') ?>
+  <?php include('./resource/header.php') ?>
+
+
+
+<!-- hero section -->
+
+<section class="hero_section hero_section_home" style="background:url('./assets/img/hero_section/hero_section.webp') no-repeat center/cover">
+<div>
+  
+<h4>
+  100% Nature
+</h4>
+
+<h2>
+Pure. Organic. Trusted
+</h2>
+<h3>
+  <span>
+  Elevating
+  </span>
+  <span>
+  Health with Nature’s
+  </span>
+</h3>
+<p>
+Dedicated to providing pure, chemical-free organic products while fostering a healthier and more sustainable way of life. We ensure quality, support local farmers, and promote eco-friendly practices for a better tomorrow."
+</p>
+
+<button>
+  Explore
+</button>
+
+</div>
+
     </section>
+
+
+
+
+
+
+
+
+
+
+
+
+
+  <section class="divison_section">
+    <?php include('./resource/division.php') ?>
+  </section>
 
   <section class="about_section_main" style="background: url('./assets/img/home/about.webp') no-repeat center center/cover;">
     <div class="about_section">
-        <div class="about_section_content">
-            <h4>
-            Nature’s Purity, Your Well-Being
-            </h4>
-            <h2>
-            Who We Are
-            </h2>
-            <p>
-            At Anbin Varam, we offer 100% organic, chemical-free products and holistic healing through our Integrated Natural Hospital. Committed to health, sustainability, and eco-friendly practices, we bring nature’s best to you.
-            </p>
-            <ul class="mt-5">
-                <li>
-                    <span>
-                    <i class="fa-solid fa-check"></i>
-                    </span>
-                <span>
-                Natural Products For
-                </span>
-                </li>
-                <li>
-                    <span>
-                    <i class="fa-solid fa-check"></i>
-                    </span>
-                    <span>
-                    Loves of Healthy Food.
-                    </span>
-                </li>
-            </ul>
-           <a href="./about.php">
-           <button>
-                <span>
-                    More
-                </span>
-                <span>
-                <i class="fa-solid fa-arrow-right"></i>
-                </span>
-            </button>
-           </a>
-        </div>
-        <div class="about_section_img">
+      <div class="about_section_content">
+        <h4>
+          Nature’s Purity, Your Well-Being
+        </h4>
+        <h2>
+          Who We Are
+        </h2>
+        <p>
+          At Anbin Varam, we offer 100% organic, chemical-free products and holistic healing through our Integrated Natural Hospital. Committed to health, sustainability, and eco-friendly practices, we bring nature’s best to you.
+        </p>
+        <ul class="mt-5">
+          <li>
+            <span>
+              <i class="fa-solid fa-check"></i>
+            </span>
+            <span>
+              Natural Products For
+            </span>
+          </li>
+          <li>
+            <span>
+              <i class="fa-solid fa-check"></i>
+            </span>
+            <span>
+              Loves of Healthy Food.
+            </span>
+          </li>
+        </ul>
+        <a href="./about.php">
+          <button>
+            <span>
+              More
+            </span>
+            <span>
+              <i class="fa-solid fa-arrow-right"></i>
+            </span>
+          </button>
+        </a>
+      </div>
+      <div class="about_section_img">
         <swiper-container init="false" class="swiper mySwiper2">
-    <swiper-slide class='swiper_slider'>
-        <img src="./assets/img/about/image_1.webp" alt="">
-    </swiper-slide>
-    <swiper-slide class='swiper_slider'>
-    <img src="./assets/img/about/image_2.webp" alt="">
-    </swiper-slide>
-    <swiper-slide class='swiper_slider'>
-    <img src="./assets/img/about/image_3.webp" alt="">
+          <swiper-slide class='swiper_slider'>
+            <img src="./assets/img/about/image_1.webp" alt="">
+          </swiper-slide>
+          <swiper-slide class='swiper_slider'>
+            <img src="./assets/img/about/image_2.webp" alt="">
+          </swiper-slide>
+          <swiper-slide class='swiper_slider'>
+            <img src="./assets/img/about/image_3.webp" alt="">
 
-    </swiper-slide>
-  </swiper-container>
-        </div>
+          </swiper-slide>
+        </swiper-container>
+      </div>
     </div>
   </section>
 
 
+  <!-- why choose -->
+  <section class="products_main_main">
+    <div class="products_main">
+      <div class="products_main_right">
 
-  <!-- banner section -->
-<section class="banner_section" style="background: url('./assets/img/home/offer_banner.webp') no-repeat center center/cover;">
-<div class="banner_section_content">
-    <h3>
-    Quality and Purity
-    </h3>
-    <h2>
-    SALE 10% OFF
-    </h2>
-    <p>
-    organic grocery items for every family
-    </p>
-   <div class="flex justify-center mt-10">
-   <a href="">
-        <button>
-            <span>
-            Shop
-            </span>
-            <span>
-            <i class="fa-solid fa-arrow-right"></i>
-            </span>
-        </button>
-    </a>
-   </div>
-</div>
-
-</section>
-
-<section class="services_section_main" style="background: url('./assets/img/main/Section_bg.webp') no-repeat center center/cover;">
-  <div class="services_section text-center ">
-    <h3 class="service_sub_head">
-      Healing Naturally, Living Fully.
-    </h3>
-    <h2 class="service_main_head">
-      Natural Healing Services
-    </h2>
-
-    <div class="services_grid">
-      <!-- Service Item -->
-      <div class="service_card">
-        <div class="service_image">
-          <img src="./assets/img/home/image_1.webp" alt="">
-        </div>
-        <div class="service_content">
-          <h2>Herbal Treatments</h2>
-          <h3>Healing with nature’s finest herbs and remedies</h3>
-        </div>
-      </div>
-
-      <div class="service_card">
-        <div class="service_image">
-          <img src="./assets/img/home/image_2.webp" alt="">
-        </div>
-        <div class="service_content">
+        <div class="products_main_container">
+          <span>
+            New Arrivals
+          </span>
           <h2>
-          Ayurvedic Therapy
+            100% <br> Pure Natural
           </h2>
-          <h3>
-          Traditional wellness solutions for a balanced life.
-          </h3>
+          <a href="">
+            <button>
+              <span>
+                Shop Now
+              </span>
+              <span>
+                <i class="fa-solid fa-arrow-right"></i>
+
+              </span>
+            </button>
+          </a>
         </div>
+
       </div>
+      <div class="products_main_content">
+        <div class="products_main_head">
+          <div>
+            <h3>
+              Natural Only
+            </h3>
+            <h4>
+              Deal of The Week
+            </h4>
+          </div>
+          <div>
+            <a href="">
+              <button>
+                <span>
+                  All Products
+                </span>
+                <span>
+                  <i class="fa-solid fa-arrow-right"></i>
 
-      <div class="service_card">
-        <div class="service_image">
-          <img src="./assets/img/home/image_3.webp" alt="">
+                </span>
+              </button>
+            </a>
+          </div>
         </div>
-        <div class="service_content">
-          <h2>
-          Nutrition & Lifestyle Guidance
+        <div class="products_main_points mt-5">
 
-          </h2>
-          <h3>
-          Personalized health plans for overall well-being.
-          </h3>
-        </div>
-      </div>
+          <div class="products_main_point">
+            <div class="products_main_img_point">
+              <span>
+                Tommato
+              </span>
+              <img src="./assets/img/products/tomotto.png" alt="">
+            </div>
+            <div class="products_main_point_content">
+              <div class="point_content_bar">
+                <input type="range" value="45" class="slider">
+                <span>
+                  <span class='sold_text'>
+                    Sold :
+                  </span>
+                  62 / 100
+                </span>
+              </div>
+              <h2>
+                Vegan Fresh Tomato
+              </h2>
+              <h3 class="price">
+                $87.00
+              </h3>
 
-      <div class="service_card">
-        <div class="service_image">
-          <img src="./assets/img/home/image_4.webp" alt="">
-        </div>
-        <div class="service_content">
-          <h2>
-          Detox & Wellness Programs
-          </h2>
-          <h3>
-          Cleanse your body naturally for better health.
-          </h3>
+            </div>
+
+          </div>
+          <div class="products_main_point">
+            <div class="products_main_img_point">
+              <span>
+                Tommato
+              </span>
+              <img src="./assets/img/products/tomotto.png" alt="">
+            </div>
+            <div class="products_main_point_content">
+              <div class="point_content_bar">
+                <input type="range" value="45" class="slider">
+                <span>
+                  <span class="sold_text">
+                    Sold :
+                  </span>
+                  62 / 100
+                </span>
+              </div>
+              <h2>
+                Vegan Fresh Tomato
+              </h2>
+              <h3 class="price">
+                $87.00
+              </h3>
+
+            </div>
+
+          </div>
+          <div class="products_main_point">
+            <div class="products_main_img_point">
+              <span>
+                Tommato
+              </span>
+              <img src="./assets/img/products/tomotto.png" alt="">
+            </div>
+            <div class="products_main_point_content">
+              <div class="point_content_bar">
+                <input type="range" value="45" class="slider">
+                <span>
+                  <span class='sold_text'>
+                    Sold :
+                  </span>
+                  62 / 100
+                </span>
+              </div>
+              <h2>
+                Vegan Fresh Tomato
+              </h2>
+              <h3 class="price">
+                $87.00
+              </h3>
+
+            </div>
+
+          </div>
+          <div class="products_main_point">
+            <div class="products_main_img_point">
+              <span>
+                Tommato
+              </span>
+              <img src="./assets/img/products/tomotto.png" alt="">
+            </div>
+            <div class="products_main_point_content">
+              <div class="point_content_bar">
+                <input type="range" value="45" class="slider">
+                <span>
+                  <span class='sold_text'>
+                    Sold :
+                  </span>
+                  62 / 100
+                </span>
+              </div>
+              <h2>
+                Vegan Fresh Tomato
+              </h2>
+              <h3 class="price">
+                $87.00
+              </h3>
+
+            </div>
+
+          </div>
+          <div class="products_main_point">
+            <div class="products_main_img_point">
+              <span>
+                Tommato
+              </span>
+              <img src="./assets/img/products/tomotto.png" alt="">
+            </div>
+            <div class="products_main_point_content">
+              <div class="point_content_bar">
+                <input type="range" value="45" class="slider">
+                <span>
+                  <span class='sold_text'>
+                    Sold :
+                  </span>
+                  62 / 100
+                </span>
+              </div>
+              <h2>
+                Vegan Fresh Tomato
+              </h2>
+              <h3 class="price">
+                $87.00
+              </h3>
+
+            </div>
+
+          </div>
+          <div class="products_main_point">
+            <div class="products_main_img_point">
+              <span>
+                Tommato
+              </span>
+              <img src="./assets/img/products/tomotto.png" alt="">
+            </div>
+            <div class="products_main_point_content">
+              <div class="point_content_bar">
+                <input type="range" value="45" class="slider">
+                <span>
+                  <span class='sold_text'>
+                    Sold :
+                  </span>
+                  62 / 100
+                </span>
+              </div>
+              <h2>
+                Vegan Fresh Tomato
+              </h2>
+              <h3 class="price">
+                $87.00
+              </h3>
+
+            </div>
+
+          </div>
         </div>
       </div>
     </div>
-  </div>
-</section>
+  </section>
+
+  <!-- banner section -->
+  <section class="banner_section" style="background: url('./assets/img/home/offer_banner.webp') no-repeat center center/cover;">
+    <div class="banner_section_content">
+      <h3>
+        Quality and Purity
+      </h3>
+      <h2>
+        SALE 10% OFF
+      </h2>
+      <p>
+        organic grocery items for every family
+      </p>
+      <div class="flex justify-center mt-10">
+        <a href="">
+          <button>
+            <span>
+              Shop
+            </span>
+            <span>
+              <i class="fa-solid fa-arrow-right"></i>
+            </span>
+          </button>
+        </a>
+      </div>
+    </div>
+
+  </section>
+
+  <section class="services_section_main" style="background: url('./assets/img/main/Section_bg.webp') no-repeat center center/cover;">
+    <div class="services_section text-center ">
+      <h3 class="service_sub_head">
+        Healing Naturally, Living Fully.
+      </h3>
+      <h2 class="service_main_head">
+        Natural Healing Services
+      </h2>
+
+      <div class="services_grid">
+        <!-- Service Item -->
+        <div class="service_card">
+          <div class="service_image">
+            <img src="./assets/img/home/image_1.webp" alt="">
+          </div>
+          <div class="service_content">
+            <h2>Herbal Treatments</h2>
+            <h3>Healing with nature’s finest herbs and remedies</h3>
+          </div>
+        </div>
+
+        <div class="service_card">
+          <div class="service_image">
+            <img src="./assets/img/home/image_2.webp" alt="">
+          </div>
+          <div class="service_content">
+            <h2>
+              Ayurvedic Therapy
+            </h2>
+            <h3>
+              Traditional wellness solutions for a balanced life.
+            </h3>
+          </div>
+        </div>
+
+        <div class="service_card">
+          <div class="service_image">
+            <img src="./assets/img/home/image_3.webp" alt="">
+          </div>
+          <div class="service_content">
+            <h2>
+              Nutrition & Lifestyle Guidance
+
+            </h2>
+            <h3>
+              Personalized health plans for overall well-being.
+            </h3>
+          </div>
+        </div>
+
+        <div class="service_card">
+          <div class="service_image">
+            <img src="./assets/img/home/image_4.webp" alt="">
+          </div>
+          <div class="service_content">
+            <h2>
+              Detox & Wellness Programs
+            </h2>
+            <h3>
+              Cleanse your body naturally for better health.
+            </h3>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <!-- info banner -->
+
+  <section class="info_banner_main" style="background: url('./assets/img/home/info_bg.webp') no-repeat center center/cover;">
+    <div class="info_banner">
+      <div class="info_banner_point">
+        <div>
+          <img src="./assets/img/icon/organic.webp" alt="">
+        </div>
+        <div>
+          <h2>
+            100%
+          </h2>
+          <h2>
+            Pure Organic
+          </h2>
+        </div>
+      </div>
+      <div class="info_banner_point">
+        <div>
+          <img src="./assets/img/icon/cannabis.webp" alt="">
+        </div>
+        <div>
+          <h2>
+            300+
+          </h2>
+          <h2>
+            Organic Products
+          </h2>
+        </div>
+      </div>
+      <div class="info_banner_point">
+        <div>
+          <img src="./assets/img/icon/costumer.webp" alt="">
+        </div>
+        <div>
+          <h2>
+            200+
+          </h2>
+          <h2>
+            Users
+          </h2>
+        </div>
+      </div>
+    </div>
+  </section>
+
+
+  <!-- testimonial -->
 
 
 
 
+  <?php include('./resource/footer.php') ?>
 
 
+  <!-- js -->
+  <script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-element-bundle.min.js"></script>
 
-
-
-    <?php include('./resource/footer.php') ?>
-
-
-    <!-- js -->
-    <script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-element-bundle.min.js"></script>
-
-    <script>
-
-         var swiper2El = document.querySelector(".mySwiper2");
+  <script>
+    var swiper2El = document.querySelector(".mySwiper2");
     Object.assign(swiper2El, {
       grabCursor: true,
       effect: "creative",
@@ -220,7 +526,7 @@ $page_image = "./assets/img/main/logo.webp";
       },
     });
     swiper2El.initialize()
-    </script>
+  </script>
 
 </body>
 
